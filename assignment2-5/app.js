a4719232c78404dc0360eb50b2b94de9866bd9f3
roger <lobochang4@gmail.com>
2020-04-23 17:03:12 +0800

    fix the error of assignment2-5

--- assignment2-5/app.js
+++ assignment2-5/app.js
@@ -1,18 +1,17 @@
 function twoSum(nums, target){
 	for(let i=0;i<nums.length; i++){
-		let j =1;
+		let j = i+1;
 		while(j<nums.length){
 			if(nums[i]+nums[j]==target){
 				return [i,j];
 			}
 			j++;
 		}
-		j++;
 	}
 
 }
 
-console.log(twoSum([4, 2, 8, 2, 5, 4], 9));
+console.log(twoSum([2, 2, 2, 2, 4, 7], 9));
 /*
 For example:
 twoSum([2, 7, 11, 15], 9);
