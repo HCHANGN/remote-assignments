68d540bb245641c8b6009d1e6af8f82c8cd0a06a
roger <lobochang4@gmail.com>
2020-04-23 17:15:37 +0800

    modify for loop of assignment2-5

--- assignment2-5/app.js
+++ assignment2-5/app.js
@@ -1,5 +1,5 @@
 function twoSum(nums, target){
-	for(let i=0;i<nums.length; i++){
+	for(let i=0;i<nums.length-1; i++){
 		let j = i+1;
 		while(j<nums.length){
 			if(nums[i]+nums[j]==target){
@@ -11,7 +11,7 @@ function twoSum(nums, target){
 
 }
 
-console.log(twoSum([2, 2, 2, 2, 4, 7], 9));
+console.log(twoSum([1, 1, 1, 1, 2, 7], 9));
 /*
 For example:
 twoSum([2, 7, 11, 15], 9);
